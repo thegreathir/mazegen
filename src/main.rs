1d4d3c6b199b089e716ef3e3d130166ea0b3fa0d
thegreathir <aaph1377@gmail.com>
2024-03-04 18:33:12 -0400

    Add path

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use rand::{rngs::ThreadRng, Rng};
 use raqote::{DrawOptions, DrawTarget, PathBuilder, SolidSource};
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet, VecDeque};
 
 struct Maze {
     width: u32,
@@ -25,10 +25,13 @@ impl Maze {
         self.open_walls.contains(&(x1, y1, x2, y2))
     }
 
-    fn is_every_cell_reachable(&self) -> bool {
+    fn is_dest_reachable(&self) -> bool {
         let mut visited = HashSet::new();
         let mut stack = vec![(0, 0)];
         while let Some((x, y)) = stack.pop() {
+            if x == self.width - 1 && y == self.height - 1 {
+                return true;
+            }
             if visited.insert((x, y)) {
                 if x > 0 && self.is_wall_open(x, y, x - 1, y) {
                     stack.push((x - 1, y));
@@ -44,52 +47,91 @@ impl Maze {
                 }
             }
         }
-        visited.len() == (self.width * self.height) as usize
+        false
     }
 
     fn gen(&mut self, rng: &mut ThreadRng) {
-        while !self.is_every_cell_reachable() {
-            loop {
+        while !self.is_dest_reachable() {
+            while !{
                 let x = rng.gen_range(0..self.width);
                 let y = rng.gen_range(0..self.height);
                 let direction = rng.gen_range(0..4);
-                if match direction {
+                match direction {
                     0 if x > 0 => self.open_wall(x, y, x - 1, y),
                     1 if x < self.width - 1 => self.open_wall(x, y, x + 1, y),
                     2 if y > 0 => self.open_wall(x, y, x, y - 1),
                     3 if y < self.height - 1 => self.open_wall(x, y, x, y + 1),
                     _ => false,
-                } {
-                    break;
                 }
-            }
+            } {}
         }
     }
 
+    fn get_short_path(&self) -> Vec<(u32, u32)> {
+        let mut visited = HashSet::new();
+        let mut queue = VecDeque::from([(0, 0)]);
+        let mut prevs = HashMap::<(u32, u32), (u32, u32)>::new();
+        while let Some((x, y)) = queue.pop_front() {
+            if x == self.width - 1 && y == self.height - 1 {
+                let mut path = vec![(x, y)];
+                prevs.remove(&(0, 0));
+                while let Some(&prev) = prevs.get(path.last().unwrap()) {
+                    path.push(prev);
+                }
+                return path.into_iter().rev().collect();
+            }
+            if visited.insert((x, y)) {
+                if x > 0 && self.is_wall_open(x, y, x - 1, y) {
+                    queue.push_back((x - 1, y));
+                    prevs.entry((x - 1, y)).or_insert((x, y));
+                }
+                if x < self.width - 1 && self.is_wall_open(x, y, x + 1, y) {
+                    queue.push_back((x + 1, y));
+                    prevs.entry((x + 1, y)).or_insert((x, y));
+                }
+                if y > 0 && self.is_wall_open(x, y, x, y - 1) {
+                    queue.push_back((x, y - 1));
+                    prevs.entry((x, y - 1)).or_insert((x, y));
+                }
+                if y < self.height - 1 && self.is_wall_open(x, y, x, y + 1) {
+                    queue.push_back((x, y + 1));
+                    prevs.entry((x, y + 1)).or_insert((x, y));
+                }
+            }
+        }
+        vec![]
+    }
+
     fn show(&self) {
-        let cell_size = 20;
+        let cell_size = 50.;
         let mut dt = DrawTarget::new(
-            self.width as i32 * cell_size,
-            self.height as i32 * cell_size,
+            self.width as i32 * cell_size as i32,
+            self.height as i32 * cell_size as i32,
         );
-        let white_solid = raqote::Source::Solid(SolidSource {
-            r: 255,
-            g: 255,
-            b: 255,
+        let background_color = raqote::Source::Solid(SolidSource {
+            r: 211,
+            g: 211,
+            b: 211,
             a: 255,
         });
-        let black_solid = raqote::Source::Solid(SolidSource {
+        let wall_color = raqote::Source::Solid(SolidSource {
             r: 0,
             g: 0,
             b: 0,
             a: 255,
         });
+        let path_color = raqote::Source::Solid(SolidSource {
+            r: 255,
+            g: 87,
+            b: 51,
+            a: 255,
+        });
         dt.fill_rect(
             0.,
             0.,
-            self.width as f32 * cell_size as f32,
-            self.height as f32 * cell_size as f32,
-            &white_solid,
+            self.width as f32 * cell_size,
+            self.height as f32 * cell_size,
+            &background_color,
             &DrawOptions::new(),
         );
 
@@ -97,55 +139,68 @@ impl Maze {
 
         for y in 0..self.height {
             for x in 0..self.width {
-                pb.move_to(x as f32 * cell_size as f32, y as f32 * cell_size as f32);
+                pb.move_to(x as f32 * cell_size, y as f32 * cell_size);
                 if x > 0 && !self.is_wall_open(x, y, x - 1, y) {
-                    pb.line_to(
-                        x as f32 * cell_size as f32,
-                        (y + 1) as f32 * cell_size as f32,
-                    );
+                    pb.line_to(x as f32 * cell_size, (y + 1) as f32 * cell_size);
                 } else {
-                    pb.move_to(
-                        x as f32 * cell_size as f32,
-                        (y + 1) as f32 * cell_size as f32,
-                    );
+                    pb.move_to(x as f32 * cell_size, (y + 1) as f32 * cell_size);
                 }
                 if y < self.height - 1 && !self.is_wall_open(x, y, x, y + 1) {
-                    pb.line_to(
-                        (x + 1) as f32 * cell_size as f32,
-                        (y + 1) as f32 * cell_size as f32,
-                    );
+                    pb.line_to((x + 1) as f32 * cell_size, (y + 1) as f32 * cell_size);
                 } else {
-                    pb.move_to(
-                        (x + 1) as f32 * cell_size as f32,
-                        (y + 1) as f32 * cell_size as f32,
-                    );
+                    pb.move_to((x + 1) as f32 * cell_size, (y + 1) as f32 * cell_size);
                 }
                 if x < self.width - 1 && !self.is_wall_open(x, y, x + 1, y) {
-                    pb.line_to(
-                        (x + 1) as f32 * cell_size as f32,
-                        y as f32 * cell_size as f32,
-                    );
+                    pb.line_to((x + 1) as f32 * cell_size, y as f32 * cell_size);
                 } else {
-                    pb.move_to(
-                        (x + 1) as f32 * cell_size as f32,
-                        y as f32 * cell_size as f32,
-                    );
+                    pb.move_to((x + 1) as f32 * cell_size, y as f32 * cell_size);
                 }
                 if y > 0 && !self.is_wall_open(x, y, x, y - 1) {
-                    pb.line_to(x as f32 * cell_size as f32, y as f32 * cell_size as f32);
+                    pb.line_to(x as f32 * cell_size, y as f32 * cell_size)
                 } else {
-                    pb.move_to(x as f32 * cell_size as f32, y as f32 * cell_size as f32);
+                    pb.move_to(x as f32 * cell_size, y as f32 * cell_size);
                 }
             }
         }
         let path = pb.finish();
         dt.stroke(
             &path,
-            &black_solid,
+            &wall_color,
             &raqote::StrokeStyle {
                 cap: raqote::LineCap::Round,
                 join: raqote::LineJoin::Round,
-                width: 3.,
+                width: 5.,
+                miter_limit: 1.,
+                dash_array: vec![],
+                dash_offset: 0.,
+            },
+            &DrawOptions::new(),
+        );
+
+        let mut pb = PathBuilder::new();
+        let short_path = self.get_short_path();
+        for (i, (x, y)) in short_path.iter().enumerate() {
+            if i == 0 {
+                pb.move_to(
+                    *x as f32 * cell_size + cell_size / 2.,
+                    *y as f32 * cell_size + cell_size / 2.,
+                );
+            } else {
+                pb.line_to(
+                    *x as f32 * cell_size + cell_size / 2.,
+                    *y as f32 * cell_size + cell_size / 2.,
+                );
+            }
+        }
+
+        let path = pb.finish();
+        dt.stroke(
+            &path,
+            &path_color,
+            &raqote::StrokeStyle {
+                cap: raqote::LineCap::Round,
+                join: raqote::LineJoin::Round,
+                width: 15.,
                 miter_limit: 1.,
                 dash_array: vec![],
                 dash_offset: 0.,
@@ -159,7 +214,7 @@ impl Maze {
 
 fn main() {
     let mut rng = rand::thread_rng();
-    let mut maze = Maze::new(20, 20);
+    let mut maze = Maze::new(50, 40);
     maze.gen(&mut rng);
     maze.show();
 }
